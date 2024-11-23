7dd40a0295b35ec76613a4514aa5a8ceeac58895
Ana Karoline <anadevkarol@gmail.com>
2024-11-23 11:58:39 -0300

    inserindo um config.json na api-gastos

---
+++ api-gastos/migrations/20241120160656-initial-migration.js
@@ -0,0 +1,31 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("expenses", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      amount: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("expenses");
+  },
+};
